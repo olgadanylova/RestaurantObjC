48de5200e47fac9245a124e68093c73038f96245
Olga Danylova <olga@themidnightcoders.com>
2018-03-22 14:25:02 +0200

    ColorHelper changed

--- restaurant/Utils/ColorHelper.m
+++ restaurant/Utils/ColorHelper.m
@@ -17,7 +17,7 @@
     NSScanner *scanner = [NSScanner scannerWithString:hexColor];
     [scanner setScanLocation:1]; // bypass '#' character
     [scanner scanHexInt:&rgbValue];
-    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:alpha];
+    return [UIColor colorWithRed:((rgbValue & 0xFF) >> 16)/255.0 green:((rgbValue & 0xFF) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:alpha];
 }
 
 @end
